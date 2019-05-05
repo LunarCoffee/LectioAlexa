67720b465861661afd88c6d56c7311df87ed4e0e
Fred G <lunarcoffee.pjc@gmail.com>
2019-05-05 06:27:24 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,210 @@
+// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+// session persistence, api calls, and more.
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Hi! You can ask me to listen to you read.';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+    }
+};
+
+const GoToPageIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'GoToPageIntent';
+    },
+    handle(handlerInput) {
+        const book = handlerInput.requestEnvelope.request.intent.slots.book.value;
+        return handlerInput.responseBuilder
+            .speak(`Okay. I'm following you in ${book}.`)
+            .reprompt("You can start reading now.")
+            .getResponse();
+    }
+};
+
+function clean(text) {
+    return text.trim().toLowerCase().replace(/[^A-Za-z0-9\s]/g,"").replace(/\s{2,}/g, " ");
+}
+
+// Page 55, Tucket's Gold
+const sampleText = `that was a good sign. ridden horses were not allowed to stop at every little bit of grass.`;
+
+// Page 63, Brian's Hunt
+const sampleText2 = "And so he packed the canoe and when it was packet he tied cord to it";
+
+function compare(str, correct) {
+    let incorrect_array = [];
+    let str_array = clean(str).split(' ');
+    
+    let correct_array = clean(correct).split(" ");
+    
+    for (let i = 0; i < correct_array.length; i++) {
+        if ((correct_array[i] === str_array[i]) ||
+            (correct_array[i - 1] === str_array[i]) ||
+            (correct_array[i + 1] === str_array[i])){
+        } else {
+            incorrect_array.push(str_array[i]);
+        }
+    }
+    
+    return incorrect_array.filter(s => s).join(", ");
+}
+
+const FromTheBookIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === "IntentRequest"
+            && handlerInput.requestEnvelope.request.intent.name === "FromTheBookIntent";
+    },
+    handle(handlerInput) {
+        const input = handlerInput.requestEnvelope.request.intent.slots.text.value;
+        let response = "Something went wrong...";
+        
+        let incorrect = compare(input, sampleText2);
+        if (incorrect.length > 0) {
+            response = `You got the words ${incorrect}, incorrect.`
+        } else {
+            response = "You read that perfectly!"
+        }
+        
+        return handlerInput.responseBuilder
+            .speak(response)
+            .getResponse();
+    }
+}
+
+const FromTheBookTucketIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === "IntentRequest"
+            && handlerInput.requestEnvelope.request.intent.name === "FromTheBookTucketIntent";
+    },
+    handle(handlerInput) {
+        const input = handlerInput.requestEnvelope.request.intent.slots.text.value;
+        let response = "Something went wrong...";
+        
+        let incorrect = compare(input, sampleText);
+        if (incorrect.length > 0) {
+            response = `You got the words, ${incorrect}, incorrect.`
+        } else {
+            response = "You read that perfectly!"
+        }
+        
+        return handlerInput.responseBuilder
+            .speak(response)
+            .getResponse();
+    }
+}
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+    }
+};
+
+const EatMyHatIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'EatMyHatIntent';
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak("Mmm. That was a good hat.")
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = 'Goodbye!';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = handlerInput.requestEnvelope.request.intent.name;
+        const speechText = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.message}`);
+        const speechText = `Sorry, I couldn't understand what you said. Please try again.`;
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .getResponse();
+    }
+};
+
+// This handler acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        GoToPageIntentHandler,
+        FromTheBookIntentHandler,
+        FromTheBookTucketIntentHandler,
+        HelpIntentHandler,
+        EatMyHatIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    .addErrorHandlers(
+        ErrorHandler)
+    .lambda();
+
